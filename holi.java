265e7cb4a0cc4b2e3636ad931420bb444a99e18d
Manikandan81480 <mani81480@gmail.com>
2017-03-13 09:45:44 -0700

    Update and rename README.md to holi.java

---
+++ holi.java
@@ -0,0 +1,20 @@
+package ivtl;
+import java.util.*;
+public class p {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner x=new Scanner(System.in);
+		String s=x.next();
+		String s2="sunday";
+		if(s.equals(s2))
+		{
+			System.out.println("false");
+		}
+		else
+		{
+		System.out.println("true");
+		}
+	}
+
+}
